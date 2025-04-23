9be9ee3137503769160b8fc49f4104b3eee76c4b
Marouane Laamiri <malaamir@e3r2p18.1337.ma>
2025-04-23 18:17:47 +0100

    zbi

--- minishell.c
+++ minishell.c
@@ -1,28 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sojammal <sojammal@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
-/*   Created: 2025/04/23 17:57:50 by sojammal          #+#    #+#             */
-/*   Updated: 2025/04/23 17:58:58 by sojammal         ###   ########.fr       */
-=======
-<<<<<<< HEAD
-/*   Created: 2025/04/13 15:01:45 by malaamir          #+#    #+#             */
-/*   Updated: 2025/04/23 17:47:07 by sojammal         ###   ########.fr       */
-=======
-/*   Created: 2025/04/23 14:14:55 by malaamir          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/04/23 17:31:40 by malaamir         ###   ########.fr       */
-=======
-/*   Updated: 2025/04/23 14:28:02 by malaamir         ###   ########.fr       */
->>>>>>> 1a8796a6b425341a194f576d94b9d8649d953114
->>>>>>> 6a125801a1c8db4a1346cba0381cc7c44c4e51d0
->>>>>>> dda574d69ecba10eaa1b9d79f4592c03409f56be
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "minishell.h"
 
@@ -93,13 +68,13 @@ int main(int argc, char **argv, char **envp)
 		if (*line)
 			add_history(line);
 		cmd = ft_process_input(line, env);
-<<<<<<< HEAD
+
 		ret = handle_builtins(cmd, &env);
 		if (ret > 0)
 			status = ret;
 		// else
 			// status = execute_cmd(cmd, env); // execute the command
-=======
+
 		if (cmd)
 		{
 			// ft_print_cmds(cmd);
@@ -110,16 +85,12 @@ int main(int argc, char **argv, char **envp)
 		// 	printf("make one lol\n");
 		//     // status = execute_cmds(cmd, env);
 		// }
-<<<<<<< HEAD
-=======
-=======
+
 		if (status < 0)
 		{
 		    // status = execute_cmds(cmd, env);
 		}
->>>>>>> 1a8796a6b425341a194f576d94b9d8649d953114
->>>>>>> 6a125801a1c8db4a1346cba0381cc7c44c4e51d0
->>>>>>> dda574d69ecba10eaa1b9d79f4592c03409f56be
+
 	}
 	free_env(env);
 	return 0;
