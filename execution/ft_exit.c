5199fff0e51b348b3b9288d587abf749c156fdfc
Marouane Laamiri <malaamir@e3r8p11.1337.ma>
2025-05-21 18:36:56 +0100

    21-MAY

--- execution/ft_exit.c
+++ execution/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: malaamir <malaamir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 19:59:58 by malaamir          #+#    #+#             */
-/*   Updated: 2025/05/21 16:54:23 by malaamir         ###   ########.fr       */
+/*   Updated: 2025/05/21 18:36:33 by malaamir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int	ft_exit(t_cmd *cmd, t_env **env)
 		exit(g_exit_status);
 	if (!ft_isllnum(arg->value))
 	{
-		print_error("minishell: exit: numeric argument required\n", arg->value);
+		print_error("exit: numeric argument required\n", arg->value);
 		exit(255);
 	}
 	if (arg->next)
-		return (print_error("minishell: exit: too many arguments\n",
+		return (print_error("exit: too many arguments\n",
 				arg->value), 1);
 	status = ft_atoll(arg->value);
 	status = (status % 256);
